e6a9e5e1ef4e9983107246c121512b9ebcf9af46
Marcel Marques <marcmatiasdev@gmail.com>
2025-11-15 16:58:08 -0300

    Update vite config and ts to alias @

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,38 @@
 import { defineConfig } from "vite";
 import path from "path";
 import { version } from "./package.json";
+import { fileURLToPath, URL } from "node:url";
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      'chartjs': 'chart.js',
-    }
-  },
-  build: {
-    minify: true,
-    lib: {
-      entry: path.resolve(__dirname, "src/main.js"),
-      name: "MCT",
-      fileName: () => `mct-${version}.js`,
-
-      formats: ['umd']
+export default defineConfig(({ command, mode }) => {
+  const isBuild = command === 'build';
+  return {
+    resolve: {
+      alias: {
+        'chartjs': 'chart.js',
+        'vue': 'vue/dist/vue.esm-bundler.js',
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      }
     },
-    rollupOptions: {
-      output: {
-        assetFileNames: `mct-${version}.[ext]`,
+    build: {
+      minify: true,
+      lib: {
+        entry: path.resolve(__dirname, "src/main.js"),
+        name: "MCT",
+        fileName: () => `mct-${version}.js`,
+
+        formats: ['umd']
+      },
+      rollupOptions: {
+        output: {
+          assetFileNames: `mct-${version}.[ext]`,
+        },
       },
     },
-  },
-  define: {
-    'process.env.NODE_ENV': '"production"',
-    '__VUE_OPTIONS_API__': true,
-    '__VUE_PROD_DEVTOOLS__': true,
-  },
+    define: {
+      'process.env.NODE_ENV': isBuild ? '"production"' : '"development"',
+      '__VUE_OPTIONS_API__': !isBuild,
+      '__VUE_PROD_DEVTOOLS__': !isBuild,
+      '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': !isBuild
+    }
+  }
 });
