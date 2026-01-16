5396367d09f2bb06ef9b2cf4e4770528d25d9a01
Marcel Marques <marcmatiasdev@gmail.com>
2026-01-16 10:43:21 -0300

    Add new commands

--- cypress/e2e/filters.cy.js
+++ cypress/e2e/filters.cy.js
@@ -1,102 +1,7 @@
 describe('Filtros - Teste de filtragens', () => {
   beforeEach(() => {
     cy.visit('/')
-
-    cy.get('body').type('{esc}');
-
-    // Seletor Doença
-    cy.get('[data-test="select-doenca"] .n-base-selection-label')
-      .should('be.visible')
-      .click();
-
-    cy.get('.n-base-select-menu:visible')
-      .contains('Caxumba')
-      .should('be.visible')
-      .click();
-
-    // Seletor Dose
-    cy.get('[data-test="select-dose"]')
-      .should('be.visible')
-      .click();
-
-    cy.get('.n-base-select-menu:visible')
-      .contains('1ª dose')
-      .should('be.visible')
-      .click();
-
-    cy.get('body').type('{esc}');
-
-    // Seletor tipo de dado
-    cy.get('[data-test="select-type"]')
-      .should('be.visible')
-      .click();
-
-    cy.get('.n-base-select-menu:visible')
-      .contains('Cobertura')
-      .should('be.visible')
-      .click();
-
-    cy.get('body').type('{esc}');
-
-    // Seletor tipo de estados
-    cy.get('[data-test="select-states"]')
-      .should('be.visible')
-      .click();
-
-    cy.get('button.n-button--small-type')
-      .should('be.visible')
-      .contains('Marcar todos')
-      .click();
-
-    cy.get('body').type('{esc}');
-
-    // Seletor tipo de ano inicial
-    cy.get('.start-datepicker')
-      .should('be.visible')
-      .click();
-
-    cy.get('.n-base-select-menu:visible')
-      .should('be.visible')
-      .within(() => {
-        cy.get('.n-scrollbar .n-virtual-list')
-          .scrollTo('top');
-
-        cy.get('div.n-base-select-option__content')
-          .contains('2000')
-          .should('be.visible')
-          .click();
-      });
-
-    cy.get('body').type('{esc}');
-
-    // Seletor tipo de ano final
-    cy.get('.end-datepicker')
-      .should('be.visible')
-      .click();
-
-    cy.get('.n-base-select-menu:visible')
-      .should('be.visible')
-      .within(() => {
-        cy.get('.n-scrollbar .n-virtual-list')
-          .scrollTo('bottom');
-
-        cy.get('div.n-base-select-option__content')
-          .contains('2024')
-          .should('be.visible')
-          .click();
-      });
-
-    cy.get('body').type('{esc}');
-
-    // Seletor tipo de granularidade
-    cy.get('[data-test="select-granularity"]')
-      .should('be.visible')
-      .click();
-
-    cy.get('.n-base-select-menu:visible')
-      .contains('Estados')
-      .should('be.visible')
-      .click();
+    cy.fillAllFilters();
   });
   it('Título e subtítulos após definição de filtragem', function() {
     // Assertions
@@ -132,6 +37,8 @@ describe('Filtros - Teste de filtragens', () => {
     cy.get('[data-test="select-dose"]').should('contain', 'Selecione');
     cy.get('[data-test="select-type"]').should('contain', 'Selecione');
     cy.get('[data-test="select-states"]').should('contain', 'Selecione');
+    cy.get('.start-datepicker') .should('be.visible').should('contain', 'Início');
+    cy.get('.end-datepicker') .should('be.visible').should('contain', 'Final');
     cy.get('[data-test="select-granularity"]').should('contain', 'Selecione');
   });
 });
--- cypress/support/component.js
+++ cypress/support/component.js
@@ -1,3 +1,4 @@
+import './commands';
 import { mount } from 'cypress/vue'
 
 Cypress.Commands.add('mount', mount)
